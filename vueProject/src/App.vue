64baeadd179192577e45cb63c77737d5c8a067fa
IDongDeShuai <>
2020-10-14 15:30:39 +0800

    1.这是单页面应用 2.里面包含了如何组件嵌套组件 3.里面注明了如何选择是否压缩代码 4.里面演示了如何使用routerview

---
+++ vueProject/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <!-- 组件嵌套组件 -->
+    <webbottom/>
+  </div>
+</template>
+
+<script>
+// 引入其他组件,因为bottom作为嵌套的组件所以无需在index.js中的router中注册
+import bottom from './components/subpage/Bottom'
+export default {
+  name: 'App',
+  // 将组件命名为webbottom使用
+  components: {
+    webbottom: bottom
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueProject/src/components/subpage/Bottom.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div class="bottom">
+    底部模板
+  </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style>
+.bottom{
+  width: 100%;
+  height: 300px;
+  background-color: aqua;
+}
+</style>
